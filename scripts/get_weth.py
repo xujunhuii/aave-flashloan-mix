44e561d48872060c9311b73c3833667a85b6e2ce
Junhui Xu <bizjun@nus.edu.sg>
2021-11-08 18:13:09 +0800

    update

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -16,6 +16,6 @@ def get_weth():
         config["wallets"]["from_key"]
     )  # add your keystore ID as an argument to this call
     weth = interface.WethInterface(config["networks"][network.show_active()]["weth"])
-    tx = weth.deposit({"from": acct, "value": 1000000000000000000})
+    tx = weth.deposit({"from": acct, "value": 1000000000000})
     print("Received 1 WETH")
     return tx
